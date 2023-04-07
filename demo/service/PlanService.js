6f13bfb6397425108b59cb14bd3448fad7039460
Akshay Soni <akshay.soni.8707@gmail.com>
2023-04-07 12:39:04 +0530

    Api endpoint updated

--- demo/service/PlanService.js
+++ demo/service/PlanService.js
@@ -3,26 +3,26 @@ import axios from 'axios';
 
 export class PlanService {
     constructor() {
-        this.contextPath = 'https://shopifyapp.iihtsrt.com/public';
+        this.contextPath = 'https://shopifyapp.iihtsrt.com/public/naturescure-api/plan/v1/';
     }
 
     async getPlans() {
-        const res = await axios.get(this.contextPath + '/plan/naturescure-api/v1/getAll');
+        const res = await axios.get(this.contextPath + 'all');
         return res.data;
     }
 
     async savePlan(plan) {
         var res = null;
         if (plan.package_id != null) {
-            res = await axios.post(this.contextPath + '/plan/naturescure-api/v1/update/' + plan.package_id, plan);
+            res = await axios.post(this.contextPath + 'update/' + plan.package_id, plan);
         } else {
-            res = await axios.post(this.contextPath + '/plan/naturescure-api/v1/insertplan', plan);
+            res = await axios.post(this.contextPath + 'add', plan);
         }
         return res.data;
     }
 
     async deletePlan(id) {
-        const res = await axios.post(this.contextPath + '/plan/naturescure-api/v1/deleteplan/' + id);
+        const res = await axios.get(this.contextPath + 'delete' + id);
         return res.data;
     }
 }
