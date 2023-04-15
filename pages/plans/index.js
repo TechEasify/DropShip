459f1e163a0dfd4108e9237bd1cb11117a67a651
Akshay Soni <akshay.soni.8707@gmail.com>
2023-04-15 08:30:03 +0530

    UI updated and MM / YY filtering logic added

--- pages/plans/index.js
+++ pages/plans/index.js
@@ -5,111 +5,103 @@ import { Button } from 'primereact/button';
 import { LayoutContext } from '../../layout/context/layoutcontext';
 import { classNames } from 'primereact/utils';
 import Link from 'next/link';
+import { PlanService } from '../../demo/service/PlanService';
+import { InputSwitch } from 'primereact/inputswitch';
+import { SelectButton } from 'primereact/selectbutton';
 
 const Plans = () => {
+    const [plans, setPlans] = useState(null);
+    const [filteredPlans, setFilteredPlans] = useState(null);
+    const [selectDurationButtonValue, setSelectDurationButtonValue] = useState(null);
     const { layoutConfig } = useContext(LayoutContext);
     const contextPath = getConfig().publicRuntimeConfig.contextPath;
     const router = useRouter();
     const containerClassName = classNames('surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden', { 'p-input-filled': layoutConfig.inputStyle === 'filled' });
+
+    useEffect(() => {
+        const planService = new PlanService();
+        planService.getPlans().then((data) => {
+            console.log(data.data);
+            setPlans(data.data);
+        });
+    }, []);
+
+    useEffect(() => {
+        setSelectDurationButtonValue({ name: 'Monthly', code: 'Month' });
+        plans && setFilteredPlans(plans.filter((plan) => plan.duration == 'Month'));
+    }, [plans]);
+
+    useEffect(() => {
+        plans && setFilteredPlans(plans.filter((plan) => plan.duration == selectDurationButtonValue.code));
+    }, [selectDurationButtonValue]);
+
+    const durationChangeHandler = (e) => {
+        setSelectDurationButtonValue(e.value);
+    };
+
+    const selectDurationButtonOptions = [
+        { name: 'Monthly', code: 'Month' },
+        { name: 'Yearly', code: 'Year' }
+    ];
+
     return (
         <>
-            <div className="text-900 font-bold text-6xl mb-4">Set up your store, pick a plan later</div>
-            <div className="text-700 text-xl mb-6 line-height-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit numquam eligendi quos.</div>
-
-            <div className="grid">
-                <div className="col-12 lg:col-4">
-                    <div className="p-3 h-full">
-                        <div className="shadow-2 p-3 h-full flex flex-column surface-card" style={{ borderRadius: '6px' }}>
-                            <div className="text-900 font-medium text-4xl mb-2">Basic</div>
-                            <div className="text-600">What's included on Basic?</div>
-                            <hr className="my-3 mx-0 border-top-1 border-none surface-border" />
-                            <div className="flex align-items-center">
-                                <span className="font-bold text-2xl text-900">$3.81</span>
-                                <span className="ml-2 font-medium text-600">per month</span>
-                            </div>
-                            <hr className="my-3 mx-0 border-top-1 border-none surface-border" />
-                            <ul className="list-none p-0 m-0 flex-grow-1">
-                                <li className="flex align-items-center mb-3">
-                                    <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                    <span>Basic reports</span>
-                                </li>
-                                <li className="flex align-items-center mb-3">
-                                    <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                    <span>Up to 1,000 inventory locations</span>
-                                </li>
-                                <li className="flex align-items-center mb-3">
-                                    <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                    <span>2 staff accounts</span>
-                                </li>
-                            </ul>
-                            <hr className="mb-3 mx-0 border-top-1 border-none surface-border mt-auto" />
-                            <Button label="Try for free" className="p-3 w-full mt-auto"></Button>
-                        </div>
+            {filteredPlans && (
+                <>
+                    <div className="text-900 font-bold text-6xl mb-4">Set up your store, pick a plan later</div>
+                    <div className="text-700 text-xl mb-6 line-height-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit numquam eligendi quos.</div>
+                    <div className="text-center my-3">
+                        <h5>Choose Your Plan</h5>
+                        <SelectButton value={selectDurationButtonValue} onChange={(e) => durationChangeHandler(e)} options={selectDurationButtonOptions} optionLabel="name" />
                     </div>
-                </div>
-
-                <div className="col-12 lg:col-4">
-                    <div className="p-3 h-full">
-                        <div className="shadow-2 p-3 h-full flex flex-column surface-card" style={{ borderRadius: '6px' }}>
-                            <div className="text-900 font-medium text-4xl mb-2">Standard</div>
-                            <div className="text-600">What's included on Standard?</div>
-                            <hr className="my-3 mx-0 border-top-1 border-none surface-border" />
-                            <div className="flex align-items-center">
-                                <span className="font-bold text-2xl text-900">$5.67</span>
-                                <span className="ml-2 font-medium text-600">per month</span>
-                            </div>
-                            <hr className="my-3 mx-0 border-top-1 border-none surface-border" />
-                            <ul className="list-none p-0 m-0 flex-grow-1">
-                                <li className="flex align-items-center mb-3">
-                                    <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                    <span>Professional reports</span>
-                                </li>
-                                <li className="flex align-items-center mb-3">
-                                    <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                    <span>Up to 1,000 inventory locations</span>
-                                </li>
-                                <li className="flex align-items-center mb-3">
-                                    <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                    <span>5 staff accounts</span>
-                                </li>
-                            </ul>
-                            <hr className="mb-3 mx-0 border-top-1 border-none surface-border" />
-                            <Button label="Try for free" className="p-3 w-full"></Button>
-                        </div>
+                    <div className="grid">
+                        {filteredPlans.map((plan, planIndex) => {
+                            return (
+                                <div key={planIndex} className="col-12 lg:col">
+                                    <div className="p-1 h-full">
+                                        <div className={`shadow-2 p-3 h-full flex flex-column ${plan.is_popular === '1' ? 'text-white bg-indigo-500' : 'bg-white'}`} style={{ borderRadius: '6px' }}>
+                                            <div className={`text-900 font-medium text-4xl mb-2 ${plan.is_popular === '1' ? 'text-white' : 'text-black'}`}>{plan.package_name}</div>
+                                            <div className={`text-600 ${plan.is_popular === '1' ? 'text-white' : 'text-black'}`}>{plan.description}</div>
+                                            <hr className="my-3 mx-0 border-top-1 border-none surface-border" />
+                                            <div className="flex align-items-center">
+                                                <span className={`font-bold text-2xl text-900  ${plan.is_popular === '1' ? 'text-white' : 'text-black'}`}>{plan.amount}</span>
+                                                <span className={`ml-2 font-medium text-600  ${plan.is_popular === '1' ? 'text-white' : 'text-black'}`}>/ month</span>
+                                            </div>
+                                            <hr className="my-3 mx-0 border-top-1 border-none surface-border" />
+                                            <ul className="list-none p-0 m-0 flex-grow-1">
+                                                <li className="flex align-items-center mb-3">
+                                                    <i className={`pi pi-check-circle mr-2  ${plan.is_popular === '1' ? 'text-green-200' : 'text-green-500'}`}></i>
+                                                    <span>{plan.trial_period_days} Days Trial Period</span>
+                                                </li>
+                                                <li className="flex align-items-center mb-3">
+                                                    <i className={`pi pi-check-circle mr-2  ${plan.is_popular === '1' ? 'text-green-200' : 'text-green-500'}`}></i> <span>{plan.max_products} Unique Products</span>
+                                                </li>
+                                                {plan.has_personalized_branding === '1' && (
+                                                    <li className="flex align-items-center mb-3">
+                                                        <i className={`pi pi-check-circle mr-2  ${plan.is_popular === '1' ? 'text-green-200' : 'text-green-500'}`}></i> <span>Personalized Branding</span>
+                                                    </li>
+                                                )}
+                                                {plan.has_branded_invoicing === '1' && (
+                                                    <li className="flex align-items-center mb-3">
+                                                        <i className={`pi pi-check-circle mr-2  ${plan.is_popular === '1' ? 'text-green-200' : 'text-green-500'}`}></i> <span>Branded Invoicing</span>
+                                                    </li>
+                                                )}
+                                                {plan.can_customize_product_images === '1' && (
+                                                    <li className="flex align-items-center mb-3">
+                                                        <i className={`pi pi-check-circle mr-2  ${plan.is_popular === '1' ? 'text-green-200' : 'text-green-500'}`}></i> <span>Customize Product Image Background</span>
+                                                    </li>
+                                                )}
+                                            </ul>
+                                            <hr className="mb-3 mx-0 border-top-1 border-none surface-border mt-auto" />
+                                            <Button label="Try for free" className={`p-3 w-full mt-auto ${plan.is_popular === '1' ? 'text-indigo-500 bg-white' : 'text-white bg-indigo-500'}`}></Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })}
                     </div>
-                </div>
-
-                <div className="col-12 lg:col-4">
-                    <div className="p-3 h-full">
-                        <div className="shadow-2 p-3 flex flex-column surface-card" style={{ borderRadius: '6px' }}>
-                            <div className="text-900 font-medium text-4xl mb-2">Premium</div>
-                            <div className="text-600">What's included on Premium?</div>
-                            <hr className="my-3 mx-0 border-top-1 border-none surface-border" />
-                            <div className="flex align-items-center">
-                                <span className="font-bold text-2xl text-900">$9.34</span>
-                                <span className="ml-2 font-medium text-600">per month</span>
-                            </div>
-                            <hr className="my-3 mx-0 border-top-1 border-none surface-border" />
-                            <ul className="list-none p-0 m-0 flex-grow-1">
-                                <li className="flex align-items-center mb-3">
-                                    <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                    <span>Custom report builder</span>
-                                </li>
-                                <li className="flex align-items-center mb-3">
-                                    <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                    <span>Up to 1,000 inventory locations</span>
-                                </li>
-                                <li className="flex align-items-center mb-3">
-                                    <i className="pi pi-check-circle text-green-500 mr-2"></i>
-                                    <span>15 staff accounts</span>
-                                </li>
-                            </ul>
-                            <hr className="mb-3 mx-0 border-top-1 border-none surface-border" />
-                            <Button label="Try for free" className="p-3 w-full mt-auto"></Button>{' '}
-                        </div>
-                    </div>
-                </div>
-            </div>
+                </>
+            )}
         </>
     );
 };
