259dc8e2fc24429e71e027378f0c6d6cea858b84
Kishan9896 <ckishan9896@gmail.com>
2024-03-29 15:47:56 +0530

    upload file changes

--- src/modules/FileLibrary/pages/UploadFile.js
+++ src/modules/FileLibrary/pages/UploadFile.js
@@ -1,213 +1,185 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './UploadFile.scss';
-import { UserGetIMG } from '../actions';
-import { UploadImage, Image, PreviewImg } from '../../../components';
+import {
+  LegacyFilters,
+  EmptyState,
+  Layout,
+  LegacyCard,
+  ResourceList,
+  IndexTable,
+  useIndexResourceState,
+  Button,
+} from '@shopify/polaris';
 
-export default function UploadFile({ isDesign, onChooseImage }) {
-  const [images, setImages] = useState([]);
-  const [myFile, setMyFile] = useState(true);
-  const [sampleFile, setSampleFile] = useState(false);
-
-  useEffect(() => {
-    UserGetIMG()
-      .then((res) => {
-        setImages(res.data.data);
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
-  const onClickHandle = (e) => {
-    const { id } = e.target.parentElement;
-
-    if (id === 'myFile') {
-      setMyFile(true);
-      setSampleFile(false);
-    } else if (id === 'sampleFile') {
-      setMyFile(false);
-      setSampleFile(true);
-    }
+export default function UploadFile() {
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [uploadedImages, setUploadedImages] = useState([]);
+  const items = [];
+  const appliedFilters = [];
+  const filters = [];
+  const resourceName = {
+    singular: 'file',
+    plural: 'files',
   };
+
+  const filterControl = (
+    <LegacyFilters
+      disabled={!items.length}
+      queryValue=""
+      filters={filters}
+      appliedFilters={appliedFilters}
+      onClearAll={() => undefined}
+      onQueryChange={() => undefined}
+      onQueryClear={() => undefined}
+    />
+  );
+
+  const handleFileUpload = (event) => {
+    const files = event.target.files;
+    const newUploadedFilesArray = [...uploadedFiles, ...files];
+    setUploadedFiles(newUploadedFilesArray);
+
+    // Create an array of URLs for the newly uploaded images
+    const newImageUrls = [];
+    for (let i = 0; i < files.length; i++) {
+      const imageUrl = URL.createObjectURL(files[i]);
+      newImageUrls.push(imageUrl);
+    }
+
+    // Append the new image URLs to the existing list
+    const updatedImageUrls = [...uploadedImages, ...newImageUrls];
+    setUploadedImages(updatedImageUrls);
+  };
+
+  const handleClick = () => {
+    const fileInput = document.getElementById('file-input');
+    fileInput.click();
+  };
+
+  const handleDownload = (file) => {
+    const url = URL.createObjectURL(file);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = file.name;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
+
+  const emptyStateMarkup =
+    !appliedFilters.length && uploadedFiles.length === 0 ? (
+      <EmptyState
+        heading="Upload a file to get started"
+        action={{ content: 'Upload files', onAction: handleClick }}
+        image="https://cdn.shopify.com/s/files/1/2376/3301/products/emptystate-files.png"
+      >
+        <p>You can use the Files section to upload images</p>
+      </EmptyState>
+    ) : (
+      <></>
+    );
+
+  const { selectedResources, allResourcesSelected, handleSelectionChange } =
+    useIndexResourceState({
+      selectedItems: uploadedFiles.map((_, index) => index),
+      resourceName: resourceName,
+      idForItem: (item, index) => index,
+    });
+
+  const rowMarkup = uploadedFiles.map((file, index) => (
+    <IndexTable.Row
+      key={`file-${index}`}
+      selected={selectedResources.includes(index)}
+    >
+      <IndexTable.Cell>
+        <div className="file-preview">
+          <img
+            src={uploadedImages[index]}
+            alt={file.name}
+            className="file-thumbnail"
+          />
+        </div>
+      </IndexTable.Cell>
+      <IndexTable.Cell>
+        <div className="file-preview">
+          <span className="file-name">{file.name}</span>
+        </div>
+      </IndexTable.Cell>
+      <IndexTable.Cell>
+        <span>{file.type}</span>
+      </IndexTable.Cell>
+      <IndexTable.Cell>
+        <div className="action-row">
+          <Button onClick={() => handleDownload(file)}>Download</Button>
+        </div>
+      </IndexTable.Cell>
+    </IndexTable.Row>
+  ));
+
+  console.log(uploadedFiles, 'uploadedFiles');
 
   return (
     <>
-      <div className="container-fluid dashboard__container pf-bg-white clearfix">
-        <h2 className="pf-h2 pf-my-40">File library</h2>
-        <div id="main-file-library">
-          <div data-content="Drop files here" className="file-library dropzone">
-            <div className="margin-bottom-20">
-              <ul className="pf-tabs primary" style={{ top: '0px' }}>
-                <div className="tab-wrap">
-                  <li
-                    id="myFile"
-                    className={`tab ${myFile ? 'active' : ''}`}
-                    onClick={onClickHandle}
-                  >
-                    <span>My files</span>
-                  </li>
-                  {/* <li
-                    id="sampleFile"
-                    className={`tab ${sampleFile ? 'active' : ''}`}
-                    onClick={onClickHandle}
-                  >
-                    <span>Sample files</span>
-                  </li> */}
-                </div>
-                <span className="nav-arrow left hidden">
-                  <i className="pf-i pf-i-chevron-left pf-i-24" />
-                </span>
-                <span className="nav-arrow right hidden">
-                  <i className="pf-i pf-i-chevron-right pf-i-24" />
-                </span>
-              </ul>
-            </div>
-            <div>
-              <div className="filter">
-                <div className="filter-store-select">
-                  <select className="styled-select">
-                    <option value="4772535">Personal orders</option>
-                  </select>
-                </div>
-                <div className="filter-search">
-                  <div style={{ minHeight: '39px' }}>
-                    <input
-                      type="text"
-                      id="library-search-1"
-                      style={{ display: 'none' }}
-                    />
-                    <div
-                      id="library-search-1_tagsinput"
-                      className="tagsinput"
-                      style={{
-                        width: 'auto',
-                        minHeight: '39px',
-                        height: '100%',
-                      }}
-                    >
-                      <div id="library-search-1_addTag">
-                        <span
-                          role="status"
-                          aria-live="polite"
-                          className="ui-helper-hidden-accessible"
-                        />
-                        <input
-                          id="library-search-1_tag"
-                          value=""
-                          data-default="Search files"
-                          className="ui-autocomplete-input"
-                          autoComplete="off"
-                          style={{
-                            color: 'rgb(102, 102, 102)',
-                            fontFamily: 'helvetica',
-                            width: '168px',
-                          }}
-                        />
+      <div className="file-upload">
+        <div className="file-head">
+          <h2 className="pf-h2">File library</h2>
+        </div>
+        <div className="upload-img">
+          <Layout>
+            <Layout.Section>
+              <LegacyCard>
+                <ResourceList
+                  emptyState={uploadedFiles.length === 0 && emptyStateMarkup}
+                  items={uploadedFiles.map((file, index) => ({
+                    id: `file-${index}`,
+                    name: file.name,
+                    imageUrl: uploadedImages[index],
+                  }))}
+                  renderItem={() => null}
+                  resourceName={resourceName}
+                  filterControl={filterControl}
+                />
+                {uploadedFiles.length > 0 && (
+                  <div className="table-upload">
+                    <div className="upload-more">
+                      <div className="uploadtable-head">
+                        <h4>Files</h4>
                       </div>
-                      <div className="tags_clear" />
+                      <div className="uploadmore-btn">
+                        <Button onClick={handleClick}>Upload more files</Button>
+                      </div>
                     </div>
+                    <IndexTable
+                      resourceName={resourceName}
+                      itemCount={uploadedFiles.length}
+                      selectedItemsCount={
+                        allResourcesSelected ? 'All' : selectedResources.length
+                      }
+                      onSelectionChange={handleSelectionChange}
+                      headings={[
+                        { title: 'File' },
+                        { title: 'Product Label' },
+                        { title: 'Type' },
+                        { title: 'Action' },
+                      ]}
+                    >
+                      {rowMarkup}
+                    </IndexTable>
                   </div>
-                </div>
-                <div className="filter-upload-methods">
-                  <UploadImage />
-                </div>
-                <div className="clearfix" />
-              </div>
-              <div className="file-mover hidden">
-                <div className="alert alert-warning">
-                  <div className="row">
-                    <div className="col-md-6 margin-top-10">
-                      Selected store is removed. You can move files to another
-                      store:
-                    </div>
-                    <div className="col-md-4">
-                      <span
-                        className="styled-select"
-                        style={{ paddingRight: '45px' }}
-                      >
-                        <select>
-                          <option data-status="active" value="4772535">
-                            Personal orders
-                          </option>
-                        </select>
-                      </span>
-                    </div>
-                    <div className="col-md-2">
-                      <a
-                        data-toggle="tooltip"
-                        data-placement="top"
-                        data-container="body"
-                        data-trigger="hover"
-                        data-original-title="Please select at least one file"
-                        href=""
-                        className="pf-btn pf-btn-secondary btn-sm btn-block"
-                      >
-                        Move
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <div>
-                  <div>
-                    <div className="text-center pf-my-24">
-                      <nav className="pf-d-inline-block" />
-                    </div>
-                  </div>
-                  <div className="row list">
-                    <div />
-                    {myFile &&
-                      images &&
-                      images.length &&
-                      images.map((image, idx) => (
-                        <Image
-                          id={image.id}
-                          key={String(idx)}
-                          src={image.url}
-                          name={image.name}
-                          size={image.size}
-                          createdAt={image.created_at}
-                          isDesign={isDesign}
-                          onChooseImage={onChooseImage}
-                        />
-                      ))}
-                    <div className="clearfix" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className={sampleFile ? '' : 'hidden'}>
-              <div>
-                <div>
-                  <div className="row list">
-                    {images && images.length && (
-                      <Image
-                        id={images[1].id}
-                        src={images[1].url}
-                        name={images[1].name}
-                        size={images[1].size}
-                        createdAt={images[1].created_at}
-                        isDesign={images[1].isDesign}
-                        onChooseImage={onChooseImage}
-                      />
-                    )}
-                  </div>
-                  <div className="clearfix" />
-                </div>
-                <div />
-              </div>
-            </div>
-          </div>
-          <ul
-            className="ui-autocomplete ui-front ui-menu ui-widget ui-widget-content ui-corner-all"
-            id="ui-id-1"
-            style={{ display: 'none' }}
-          />
+                )}
+              </LegacyCard>
+              <input
+                type="file"
+                id="file-input"
+                accept="image/*"
+                multiple
+                style={{ display: 'none' }}
+                onChange={handleFileUpload}
+              />
+            </Layout.Section>
+          </Layout>
         </div>
       </div>
-      <div className="conta-iner-fluid dashboard__container clearfix embedded-overlay" />
     </>
   );
 }
